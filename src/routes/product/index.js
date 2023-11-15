1886ea707c864f2e6704c53313c18ef962c4f870
DucHuy2801 <theflash28012002@gmail.com>
2023-11-16 00:34:29 +0700

    add authentication

--- src/routes/product/index.js
+++ src/routes/product/index.js
@@ -3,13 +3,12 @@
 const express = require('express')
 const productController = require('../../controllers/product.controller')
 const router = express.Router()
-const {asyncHandler} = require('../../auth/checkAuth')
-const { authenticationV2 } = require('../../auth/authUtils')
+const { asyncHandler } = require('../../helpers/asyncHandler');
+const { authentication } = require('../../auth/authUtils')
 
 
 // authentication
-// router.use(authenticationV2)
-
+router.use(authentication)
 router.post('', asyncHandler(productController.createProduct))
 
 module.exports = router
