3b9c8782fcb6e6b09042d0b990b1f4b21fd7ac66
DucHuy2801 <theflash28012002@gmail.com>
2023-11-17 02:20:45 +0700

    add drafts, publish, unpulish

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -21,6 +21,61 @@ class ProductController {
             })
         }).send(res)
     }
+
+    publishProductByShop = async (req, res, next) => {
+        new SuccessResponse({
+            message: 'Publish product by shop successfully!',
+            metadata: await ProductServiceV2.publishProductByShop({
+                product_id: req.params.id,
+                product_shop: req.user.userId
+            })
+        }).send(res)
+    }
+
+    unPublishProductByShop = async (req, res, next) => {
+        new SuccessResponse({
+            message: 'Unpublish product by shop successfully!',
+            metadata: await ProductServiceV2.unPublishProductByShop({
+                product_id: req.params.id,
+                product_shop: req.user.userId
+            })
+        }).send(res)
+    }
+ 
+     // QUERY //
+     /**
+      * @desc Get all Drafs for shop
+      * @param { Number } limit
+      * @param { Number } skip
+      * @return { JSON }
+      */
+    getAllDraftsForShop = async (req, res, next) => {
+        new SuccessResponse({
+            message: 'Get list Draft successfully!',
+            metadata: await ProductServiceV2.findAllDraftsForShop({
+                product_shop: req.user.userId
+            })
+        }).send(res)
+    }
+
+
+
+    getAllPublishForShop = async (req, res, next) => {
+        new SuccessResponse({
+            message: 'Get list Draft successfully!',
+            metadata: await ProductServiceV2.findAllPublishForShop({
+                product_shop: req.user.userId
+            })
+        }).send(res)
+    }
+
+    getListSearchProduct = async (req, res, next) => {
+        new SuccessResponse({
+            message: 'Get list search Product successfully!',
+            metadata: await ProductServiceV2.searchProducts(req.params)
+        }).send(res)
+    }
+    // END QUERY //
 }
 
 module.exports = new ProductController()
