4272e404a2187262c0c84c4b9214aa83c33b4ddf
DucHuy2801 <theflash28012002@gmail.com>
2023-11-16 11:32:34 +0700

    add product_shop

--- src/models/product.model.js
+++ src/models/product.model.js
@@ -24,7 +24,7 @@ const clothingSchema = new Schema({
     branch: {type: String, require: true},
     size: String,
     material: String,
-    // product_shop: {type: Schema.Types.ObjectId, ref: 'Shop'}
+    product_shop: {type: Schema.Types.ObjectId, ref: 'Shop'}
 }, {
     collection: 'clothes',
     timestamps: true
@@ -46,7 +46,7 @@ const furnitureSchema = new Schema({
     branch: {type: String, require: true},
     size: String,
     material: String,
-    // product_shop: {type: Schema.Types.ObjectId, ref: 'Shop'}
+    product_shop: {type: Schema.Types.ObjectId, ref: 'Shop'}
 }, {
     collection: 'furnitures',
     timestamps: true
