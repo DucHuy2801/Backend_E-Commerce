7fee77f9e23b398d3127e1db5f54ac65c918c9bf
DucHuy2801 <theflash28012002@gmail.com>
2023-11-19 21:50:39 +0700

    add convertToObjectIdMongoDB

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,6 +1,9 @@
 'use strict'
 
 const _ = require('lodash')
+const { Types } = require("mongoose")
+
+const convertToObjectIdMongoDB = id => new Types.ObjectId(id)
 
 const getInfoData = ({fields = [], object = {} }) => {
     return _.pick(object, fields)
@@ -44,5 +47,6 @@ module.exports = {
     getSelectData,
     unGetSelectData,
     removeUnderfinedObject,
-    updateNestedObjectParser
+    updateNestedObjectParser,
+    convertToObjectIdMongoDB
 }
